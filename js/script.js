cad7d2c8c2e9b8ed7599d80c111eef4c30ad5512
Mirko Moschella <shary.mirko@gmail.com>
2022-03-15 15:22:27 +0100

    implemento il v-for

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        arrContact: [
+            {
+                name: 'Voldemort',
+                lastAccess: '13:34',
+                img: 'img/avatar_1.jpg'
+            },
+            {
+                name: 'Dart Vather (Your Father)',
+                lastAccess: '13:34',
+                img: 'img/avatar_2.jpg'
+            },
+            {
+                name: 'Buffy the Slayer',
+                lastAccess: '13:34',
+                img: 'img/avatar_3.jpg'
+            },
+            {
+                name: 'Naraku',
+                lastAccess: '13:34',
+                img: 'img/avatar_4.jpg'
+            },
+            {
+                name: 'Mario',
+                lastAccess: '13:34',
+                img: 'img/avatar_5.jpg'
+            },
+            {
+                name: '420',
+                lastAccess: '13:34',
+                img: 'img/avatar_6.jpg'
+            },
+        ]
+    }
+});
