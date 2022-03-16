e24257505c3cb49265e2f89e1db2f0471ec07489
Mirko Moschella <shary.mirko@gmail.com>
2022-03-16 19:42:49 +0100

    implemento "sta scrivendo" e aggiorno l'ultimo accesso

--- js/script.js
+++ js/script.js
@@ -35,6 +35,12 @@ const app = new Vue({
                         status: 'recived',
                         menu: false
                     },
+                    {
+                        text: 'Click destro per il menu!',
+                        data: '12:34',
+                        status: 'recived',
+                        menu: false
+                    },
                 ],
                 visibility: true
             }, 
@@ -46,12 +52,14 @@ const app = new Vue({
                     {
                         text: 'Secondo me non dobbiamo inquinare',
                         data: '12:34',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: false
                     },
                     {
                         text: 'Hai ragione Mirko. Però come criterio di massima, come sistema di riferimento, come atteggiamento preferenziale: tu fatti i cazzi toi.',
                         data: '12:34',
-                        status: 'recived'
+                        status: 'recived',
+                        menu: false
                     },
                     
                 ],
@@ -65,17 +73,20 @@ const app = new Vue({
                     {
                         text: 'Hey! I am your father',
                         data: '12:34',
-                        status: 'recived'
+                        status: 'recived',
+                        menu: false
                     },
                     {
                         text: 'No way!',
                         data: '12:35',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: false
                     },
                     {
                         text: 'Ask your mom!',
                         data: '12:36',
-                        status: 'recived'
+                        status: 'recived',
+                        menu: false
                     },
                 ],
                 visibility: true
@@ -88,22 +99,26 @@ const app = new Vue({
                     {
                         text: 'I love you!',
                         data: '12:34',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: false
                     },
                     {
                         text: 'Are you a vampire?',
                         data: '12:35',
-                        status: 'recived'
+                        status: 'recived',
+                        menu: false
                     },
                     {
                         text: 'No!',
                         data: '12:36',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: false
                     },
                     {
                         text: 'Then you are not my type!',
                         data: '12:36',
-                        status: 'recived'
+                        status: 'recived',
+                        menu: false
                     },
                 ],
                 visibility: true
@@ -116,17 +131,20 @@ const app = new Vue({
                     {
                         text: 'Are u available?',
                         data: '12:34',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: false
                     },
                     {
                         text: 'See you in 15 min',
                         data: '12:34',
-                        status: 'recived'
+                        status: 'recived',
+                        menu: false
                     },
                     {
                         text: 'Awesome!',
                         data: '12:34',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: false
                     }
                 ],
                 visibility: true
@@ -160,6 +178,7 @@ const app = new Vue({
         answer(index){
             if(this.newText.text != '')
             {
+            this.arrContact[index].lastAccess = 'Sta scrivendo...'
             setTimeout(() => {
             let randomIndex = Math.floor(Math.random() * this.arrRandomText.length)
             this.newText.text = this.arrRandomText[randomIndex]
@@ -168,7 +187,9 @@ const app = new Vue({
             this.newText.status = 'recived'
             this.arrContact[index].chat.push({...this.newText})
             this.newText.text = ''
+            this.arrContact[index].lastAccess = this.newText.data
             }, 3000)
+            
         }},
         search() {
             this.arrContact.forEach((element) => {
